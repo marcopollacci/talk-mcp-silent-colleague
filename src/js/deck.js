0708aa723dbda37f2d825077416e97dfe63dca50
Marco Pollacci <16632565+marcopollacci@users.noreply.github.com>
2026-05-13 18:55:25 +0200

    Initial commit

---
+++ src/js/deck.js
@@ -0,0 +1,13 @@
+const styleHeader = document.createElement("style");
+styleHeader.innerHTML = `header { display: flex;
+  align-items: center;
+  gap: 0.4em;
+  :nth-child(3) {
+    margin-inline-end: -0.35em;
+  }
+ }`;
+
+window.onload = () => {
+  const headerSpeackerAside = document.querySelector("p-deck");
+  headerSpeackerAside.shadowRoot.appendChild(styleHeader);
+};
